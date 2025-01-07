ecdc0d9ba719f7bb283924b12e2069b1c48e0a82
Al-AminJoy <alaminislam3555@gmail.com>
2025-01-07 18:40:12 +0600

    Initial commit

---
+++ app/src/main/java/com/alamin/myapplication/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.alamin.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
+
+private const val TAG = "MainWheelActivity"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var wheel: ImageView
+    private val sectors: IntArray = intArrayOf(5,4,3,2,1,8,7,6)
+    lateinit var sectorDegrees: IntArray
+    private var randomSectorIndex = 0
+    private var spinning = false
+
+
+    private val random = Random()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        wheel = findViewById(R.id.wheel)
+        sectorDegrees = IntArray(sectors.size)
+        generateSectorDegrees()
+
+        wheel.setOnClickListener {
+            if (!spinning) {
+                spin()
+                spinning = true
+            }
+        }
+    }
+
+    private fun spin() {
+        //get any random sector index
+        //Change Here If you want to set a particular sector. Ex. 3,It will show 1
+        randomSectorIndex = random.nextInt(sectors.size)// the bound in exclusive
+        //generate a random degree to spin the wheel
+        val randomDegree: Int = generateRandomDegreeToSpinTo()
+
+        //do actual spinning use the rotation animation
+
+        val rotateAnimation = RotateAnimation(
+            0f,
+            randomDegree.toFloat(),
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        //time for spinning
+        rotateAnimation.duration = 4000 //3.6 sec
+        rotateAnimation.fillAfter = true // filter
+        rotateAnimation.interpolator = DecelerateInterpolator() // start with high speed, slow down at the end
+
+        rotateAnimation.setAnimationListener(object :Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val point = sectors[sectors.size - (randomSectorIndex+1)]
+                Toast.makeText(this@MainActivity, "$point", Toast.LENGTH_SHORT).show()
+                spinning = false
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+        })
+
+        wheel.startAnimation(rotateAnimation)
+
+
+    }
+
+    private fun generateRandomDegreeToSpinTo(): Int {
+        return (360 * sectors.size) + sectorDegrees[randomSectorIndex]
+    }
+
+    private fun generateSectorDegrees() {
+        val sectorDegree = 360 / sectors.size
+        Log.d(TAG, "generateSectorDegrees: Degree $sectorDegree")
+        for (i in 0 until sectors.size) {
+            sectorDegrees[i] = (i+1) * sectorDegree
+            Log.d(TAG, "generateSectorDegrees: "+sectorDegrees[i])
+        }
+
+    }
+}
